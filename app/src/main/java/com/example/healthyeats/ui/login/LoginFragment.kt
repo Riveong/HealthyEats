0cf1f43814db4e0a113e963e0724bad72c5f0a7f
malik188 <muhammad.malik01@ui.ac.id>
2023-05-31 12:26:30 +0700

    login & register implementation

---
+++ app/src/main/java/com/example/healthyeats/ui/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.healthyeats.ui.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.healthyeats.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val loginViewModel =
+            ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/healthyeats/ui/register/RegisterFragment.kt
@@ -0,0 +1,35 @@
+package com.example.healthyeats.ui.register
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.healthyeats.databinding.FragmentRegisterBinding
+import com.example.healthyeats.ui.login.LoginViewModel
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val registerViewModel =
+            ViewModelProvider(this).get(RegisterViewModel::class.java)
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
